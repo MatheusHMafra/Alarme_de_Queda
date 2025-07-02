24fc486efcfeffcf87bb6e5612fc1e6272620039
Matheus <maframatheus982@gmail.com>
2025-07-02 20:27:24 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'alarme_de_queda',
+  webDir: 'www'
+};
+
+export default config;
